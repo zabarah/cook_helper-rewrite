091d946e805a80b8b872f3e33bb28e934fea7718
zabarah <saifzabarah@gmail.com>
2016-11-19 13:55:18 -0500

    changes

--- app/src/main/java/com/example/huang/cookhelper/Main3Activity.java
+++ app/src/main/java/com/example/huang/cookhelper/Main3Activity.java
@@ -13,7 +13,7 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
     }
     public void addbutton(View view){
-        Intent i = new Intent(getApplicationContext(), Main2Activity.class);
-        getApplicationContext().startActivity(i);
+        Intent i = new Intent(Main3Activity.this, Main2Activity.class);
+        Main3Activity.this.startActivity(i);
     }
 }
