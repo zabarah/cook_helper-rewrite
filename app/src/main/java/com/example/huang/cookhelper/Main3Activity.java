e3a58ba8a5f8c6d793ed4a2b3d99d716d661e380
michael <jinyanghuangjj@gmail.com>
2016-11-19 00:10:30 -0500

    go to search page from main paage

--- app/src/main/java/com/example/huang/cookhelper/Main3Activity.java
+++ app/src/main/java/com/example/huang/cookhelper/Main3Activity.java
@@ -1,7 +1,9 @@
 package com.example.huang.cookhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+    public void openSearch(View view){
+        Intent search=new Intent(this,MainActivity.class);
+        startActivity(search);
+
     }
 }
