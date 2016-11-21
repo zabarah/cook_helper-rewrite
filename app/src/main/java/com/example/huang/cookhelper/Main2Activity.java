e15ba80687b090fd486c814232929d3747d281c0
zabarah <saifzabarah@gmail.com>
2016-11-21 13:17:11 -0500

    added ingredient list

--- app/src/main/java/com/example/huang/cookhelper/Main2Activity.java
+++ app/src/main/java/com/example/huang/cookhelper/Main2Activity.java
@@ -1,10 +1,12 @@
 package com.example.huang.cookhelper;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -33,10 +35,12 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
         EditText eTextcook = (EditText) findViewById(R.id.cooktime);
         EditText eTextcal = (EditText) findViewById(R.id.editText8);
         EditText eTextstep = (EditText) findViewById(R.id.editText2);
+        Spinner category = (Spinner) findViewById(R.id.category);
+        Spinner type = (Spinner) findViewById(R.id.type);
 
+        System.out.println(type.getSelectedItem().toString());
 
-        Recipe addrecipe=new Recipe((eTextname.getText().toString()),null, null, null,null, (eTextcook.getText().toString()),(eTextprep.getText().toString()),(eTextcal.getText().toString()),(eTextstep.getText().toString()));
-
+        Recipe addrecipe=new Recipe((eTextname.getText().toString()), "main class", type.getSelectedItem().toString(), category.getSelectedItem().toString(),null, (eTextcook.getText().toString()),(eTextprep.getText().toString()),(eTextcal.getText().toString()),(eTextstep.getText().toString()));
 
     }
     //method to execute arithmetic oper
