3ee09b97feedf1396fe7650217dbd0ab063405ad
Kevin Le <kevin.kent.le@gmail.com>
2016-12-01 11:44:32 -0500

    changes added to mysqlitehelper to create recipes table

--- app/src/main/java/com/example/huang/cookhelper/MySQLiteHelper.java
+++ app/src/main/java/com/example/huang/cookhelper/MySQLiteHelper.java
@@ -9,7 +9,14 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
         public static final String TABLE_RECIPES = "recipes";
         public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_RECIPE = "recipe";
+        public static final String COLUMN_CLASSR = "classr";
+        public static final String COLUMN_TYPE = "type";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_ING = "ing";
+        public static final String COLUMN_CAL = "cal";
+        public static final String COLUMN_COOK = "cook";
+        public static final String COLUMN_PREP = "prep";
+        public static final String COLUMN_STEPS = "steps";
 
         private static final String DATABASE_NAME = "recipes.db";
         private static final int DATABASE_VERSION = 1;
@@ -17,8 +24,12 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         // Database creation sql statement
         private static final String DATABASE_CREATE = "create table "
                         + TABLE_RECIPES + "( " + COLUMN_NAME
-                        + " text primary key autoincrement, " + COLUMN_RECIPE
-                        + " text not null);";
+                        + " text primary key autoincrement, " + COLUMN_CLASSR
+                        + " text not null, " + COLUMN_TYPE + " text not null, "
+                        + COLUMN_CATEGORY + " text not null, " + COLUMN_ING
+                        + " text not null, " + COLUMN_CAL + " text not null, "
+                        + COLUMN_COOK + " text not null, "+ COLUMN_PREP
+                        + " text not null, " + COLUMN_STEPS + " text not null" + ");";
 
         public MySQLiteHelper(Context context) {
                 super(context, DATABASE_NAME, null, DATABASE_VERSION);
